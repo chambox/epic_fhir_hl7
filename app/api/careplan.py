df131c1fd463b077cace3da7e2e5c0387f890ca3
Cyril Tata <cyril.tata@gmail.com>
2024-08-02 11:48:13 +0200

    add category types to API doc

--- app/api/careplan.py
+++ app/api/careplan.py
@@ -8,17 +8,28 @@ api = Namespace("careplan", description="CarePlan related operations")
 parser = reqparse.RequestParser()
 parser.add_argument('patient', required=False, location='args', type=str, help="Please provide the patient ID as a string")
 parser.add_argument('category', required=True, location='args')
+parser.add_argument('encounter', location='args')
 
 @api.route("/search")
 class CarePlanSearch(Resource):
     #@api.marshal_list_with(patient_list_model)
     @api.doc(params={
-        'category': 'Type of plan. 38717003 should be used to search for longitudinal care plans',
-        'patient': 'The ID of the patient',
+        'category': {'required': True, 'description': """Types of plan:
+            - **care-path**: Care path 
+            - **38717003**:  Longitudinal care plans. Patient: e0yyMZa5aqjpDhvmIODSJgw3
+            - **738906000**: Dental plan. Patient: evsJR7wUkBvPXgtZSwj8jNQ3
+            - **734163000**: Encounter Level care plan. Patient: erXuFYUfucBZaryVksYEcMg3 Encounter: elMz2mwjsRvKnZiR.0ceTUg3
+            - **inpatient-pathway**: Inpatient pathway
+            - **736353004**: Inpatient care plan (record artifact). Patient: eTRO.ZVFuhUxXw6HzTAjtfg3
+            - **736378000**: Oncology care plan. Patient: ePGJfjiWayus47VRmNLCBcA3
+            - **736271009**: Outpatient care plans. Patient: egxlDFt6t8n-cRqg1k0NObg3
+            - **409073007**: Patient Education CarePlan. Patient: eTRO.ZVFuhUxXw6HzTAjtfg3
+            """},
+        'patient': {'required': True, 'description': 'The ID of the patient'},
+        'encounter': {'description': 'Encounter ID in case of Encounter level careplan'}
     })
     def get(self):
         try:
-
             fs = FhirService()
             return fs.search_careplan(parser.parse_args(strict=True))
         except FhirServiceAuthenticationException as auth_exp:
